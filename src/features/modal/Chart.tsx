2015d5664b764d96789bc02cc3b7b6be08e67f10
KestutisRuockus <kestutisruockus@gmail.com>
2025-06-12 22:17:09 +0300

    implement dynamic chart range selection and update API fetch logic

--- src/features/modal/Chart.tsx
+++ src/features/modal/Chart.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useLayoutEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import {
   LineChart,
   Line,
@@ -11,26 +11,46 @@ import {
   ResponsiveContainer,
 } from "recharts";
 import LoadingSpinner from "../../components/ui/LoadingSpinner";
+import { FilterBarContext } from "../../context/FilterBarContext";
+import { popularCurrencies } from "../../data/currenciesList";
 
 type ChartData = {
   date: number;
   price: number;
 };
 
-const ChartComponent = ({ coinId }: { coinId: string }) => {
+const ChartComponent = ({
+  coinId,
+  daysInterval,
+}: {
+  coinId: string;
+  daysInterval: number;
+}) => {
   const [chartData, setChartData] = useState<ChartData[]>([]);
   const [loadingChart, setLoadingChart] = useState<boolean>(true);
+
+  const filterContext = useContext(FilterBarContext);
+  if (!filterContext) {
+    throw new Error(
+      "useFilterBarContext must be used within FilterBarProvider"
+    );
+  }
+  const { currency } = filterContext;
+  const symbol =
+    popularCurrencies.find((item) => item.code === currency)?.symbol ?? "";
 
   useEffect(() => {
     if (!coinId) return;
 
     setLoadingChart(true);
+    const url = import.meta.env.VITE_BASE_URL;
+
     const fetchChartData = () => {
       axios
-        .get(`https://api.coingecko.com/api/v3/coins/${coinId}/market_chart`, {
+        .get(`${url}/coins/${coinId}/market_chart`, {
           params: {
-            vs_currency: "usd",
-            days: 7,
+            vs_currency: currency,
+            days: daysInterval,
             interval: "daily",
           },
         })
@@ -50,7 +70,7 @@ const ChartComponent = ({ coinId }: { coinId: string }) => {
     };
 
     fetchChartData();
-  }, [coinId]);
+  }, [coinId, currency, daysInterval]);
 
   if (loadingChart) return <LoadingSpinner />;
 
@@ -74,7 +94,7 @@ const ChartComponent = ({ coinId }: { coinId: string }) => {
           tickFormatter={(value: number) => `$${value.toFixed(2)}`}
         />
         <Tooltip
-          formatter={(value: number) => `$${value.toFixed(2)}`}
+          formatter={(value: number) => `${symbol}${value.toFixed(2)}`}
           labelFormatter={(label: number) => {
             const date = new Date(label);
             const day = date.getDate().toString().padStart(2, "0");
@@ -82,7 +102,7 @@ const ChartComponent = ({ coinId }: { coinId: string }) => {
             const year = date.getFullYear();
             const weekday = date.toLocaleDateString("en-US", {
               weekday: "long",
-            }); // e.g., Monday
+            });
 
             return `${day}/${month}/${year} (${weekday})`;
           }}
@@ -95,11 +115,36 @@ const ChartComponent = ({ coinId }: { coinId: string }) => {
 };
 
 const Chart = ({ coinId }: { coinId: string }) => {
-  useLayoutEffect(() => {}, []);
+  const possibleDaysIntervals = [7, 14, 30];
+  type DaysInterval = (typeof possibleDaysIntervals)[number];
+  const [daysInterval, setDaysInterval] = useState<DaysInterval>(
+    possibleDaysIntervals[0]
+  );
 
   return (
-    <div className="w-full h-full">
-      <ChartComponent coinId={coinId} />
+    <div className="w-full h-full pb-6 md:pb-0">
+      <ChartComponent coinId={coinId} daysInterval={daysInterval} />
+      <div className="flex justify-center gap-4">
+        <div className="flex justify-center items-center gap-2">
+          <p className="text-[var(--color-text-primary)] font-semibold">
+            Period (days):
+          </p>
+          {possibleDaysIntervals.map((interval) => (
+            <div
+              key={interval}
+              onClick={() => setDaysInterval(interval)}
+              className={`w-10 text-center border-2 border-[var(--color-bg-primary)] ${
+                interval === daysInterval
+                  ? "bg-[var(--color-bg-primary)] font-bold"
+                  : "bg-[var(--color-bg-secondary)] hover:opacity-60"
+              } text-[var(--color-text-primary)] rounded-lg px-1.5
+            cursor-pointer transition-all duration-500`}
+            >
+              {interval}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
